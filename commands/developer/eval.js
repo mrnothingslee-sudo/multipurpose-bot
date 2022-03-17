abf31d60161e4311ba1c79327b7f7789b0c5f412
mrnothingslee-sudo <67931916+mrnothingslee-sudo@users.noreply.github.com>
2022-03-16 22:05:50 -0400

    Update eval.js

--- commands/developer/eval.js
+++ commands/developer/eval.js
@@ -1,11 +1,3 @@
-module.exports = ({
-    name: "eval",
-    aliases: ['e'],
-    description: "Evaluate a code/function",
-    usage: "eval <code/function>",
-    category: "developer",
-    code: `$eval[$message]
-    $onlyIf[$message[1]!=;{description: Add a message!}{color:RANDOM}]
-    $onlyIf[$checkContains[$channelType;text;news]==true;{description: Please do this in a guild!}{color:RANDOM}]
-    $onlyForIDs[$botOwnerID;{description:❌ You are not my Developer}{color:RANDOM}]`
-})
+m6gc-msbq
+https://discord.gg/MgCURmHZ
+https://discord.gg/mnZnFhN9
